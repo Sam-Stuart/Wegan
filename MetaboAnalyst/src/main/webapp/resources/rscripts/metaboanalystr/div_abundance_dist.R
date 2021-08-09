8f46b25d9b204cfa4d1328db11d500cd3899fafa
Violet99999 <shiyang1@ualberta.ca>
2021-08-09 13:16:24 -0600

    Abundance_Distribution_plot_not_shown

--- MetaboAnalyst/src/main/webapp/resources/rscripts/metaboanalystr/div_abundance_dist.R
+++ MetaboAnalyst/src/main/webapp/resources/rscripts/metaboanalystr/div_abundance_dist.R
@@ -2,117 +2,60 @@
 #'@description Perform rarefaction technique 
 #'@param mSetObj Input name of the created mSet Object
 #'@param data Boolean for which data set to use, normalized (default) or original
-#'@param type Set type for different methods for abundance models, drop down options are "Fisher's logseries" (default), "Split or Keep" or "Maximize"
-#'@param community Input for abundance model, drop down options are "Whole Dataset" (default), or "colSums": form row and column sums and means for numeric arrays or data frames
-#'@param row_num Boolean for the beginning (first) of selected rows, numeric data
-#'@param row_num1 Boolean for the end (last) of selected rows, numeric data 
-#'@param col_num Boolean for the beginning (first) of selected columns, numeric data
-#'@param col_num1 Boolean for the end (last) of selected columns, numeric data 
-#'@param tiesplit Input split frequencies, drop down options are "TRUE" or "FALSE" (default)
+#'@param community Input for abundance model, textbox with default as the whole dataset, or a row number (each row should represent a plot/site/sample unit)
+#'@param tiesplit Boolean for split frequencies, drop down options are "TRUE" or "FALSE" (default)
 #'@param truncate Boolean for truncation point for log-Normal model, in log2 units, default value is -1
-#'@param MARGIN Input the index is computed, drop down options are "Rows" (default), "Columns" or "Columns & Rows".
 #'@author Shiyang Zhao\email{shiyang1@ualberta.ca}
 #'University of Alberta, Canada
 #'License: GNU GPL (>= 2) ######
 #'@export
 
-#data("BCI")
-
-#mSetObj=list()
-#mSetObj$dataSet$norm=BCI
-#mSetObj$dataSet$orig=BCI
-#data = "false"
-#type = "NULL"
-#community = ""
-#row_num = ""
-#row_num1 = ""
-#col_num = ""
-#col_num1 = ""
-#tiesplit = "false"
-#truncate = ""
-#margin = "NULL" 
-
 AbundanceModel <- function(mSetObj = NA, data = "false", community = "", tiesplit = "false", truncate = ""){
-                            
+  print("start model")
+  options(errors = traceback)                          
   #library("ade4")
   #library("adegraphics")
   library("plyr")
   library("dplyr")
   library("vegan")
+
+
+  mSetObj <- .get.mSet(mSetObj)
   
-  #Extract input from mSetObj
-  
-  #FOR TESTING
-  #metaData <- "NULL"
-  #metaData <- .readDataTable("/home/louisa/Wegan/Wegan/MetaboAnalyst/src/main/webapp/resources/rscripts/metaboanalystr/test_data/dune_meta.csv")
-  #envData <- .readDataTable("/home/louisa/Wegan/Wegan/MetaboAnalyst/src/main/webapp/resources/rscripts/metaboanalystr/test_data/dune_env.csv")
-  #envData$Moisture <- as.numeric(as.character(envData$Moisture)) #Require more than one numeric envData column for numeric type option
-  
+  print("setup normal")
   if (data == "false") { #normalized data as input
     input <- mSetObj$dataSet$norm
   } else { #original data as input
     input <- mSetObj$dataSet$orig
   }
   
-#  if (margin == "NULL") {
-#    if (row_num == "") {
-#      input.2 <- select_if(input, is.numeric)
-#    } else { 
-#      if (row_num1 == "") {
-#      row_num <- as.numeric(row_num)
-#      input.2 <- select_if(input[row_num, ], is.numeric)
-#      } else {
-#      row_num <- as.numeric(row_num)
-#      row_num1 <- as.numeric(row_num1)
-#      input.2 <- select_if(input[row_num:row_num1, ], is.numeric)
-#     }
-#   }
-#  } else if (margin == "Columns") {
-#    if (col_num == "") {
-#      input.2 <- select_if(input, is.numeric)
-#    } else { 
-#      if (col_num1 == "") {
-#        col_num <- as.numeric(col_num)
-#        input.2 <- select_if(input[, col_num], is.numeric)
-#      } else {
-#        col_num <- as.numeric(col_num)
-#        col_num1 <- as.numeric(col_num1)
-#        input.2 <- select_if(input[, col_num:col_num1], is.numeric)
-#      }
-#    }
-#  } else if (margin == "Columns & Rows") {
-#      col_num <- as.numeric(col_num)
-#      col_num1 <- as.numeric(col_num1)
-#      row_num <- as.numeric(row_num)
-#      row_num1 <- as.numeric(row_num1)
-#      input.2 <- select_if(input[row_num:row_num1, col_num:col_num1], is.numeric)
-#  }
-  
   if (tiesplit == "false") {
     tiesplit1 = FALSE
   } else {
     tiesplit = TRUE
   }
-  
+  print(tiesplit)  
+
   if(truncate == "")  {
     truncate1 = -1
   } else {
     truncate1 = as.numeric(truncate)
   }
-  
+  print(truncate)  
+
   if(community == "") {
     input.c <- input.2
   } else {
     community1 <- as.numeric(community)
     input.c <- input.2[community1, ]
   }
+  print(community)
   
-  output.fisher <- fisherfit(input.2)
-  output.fit <- prestonfit(input.2, tiesplit = tiesplit1)
-  output.distr <- prestondistr(input.2, truncate = truncate1)
+  output.fisher <- fisherfit(input.c)
+  output.fit <- prestonfit(input.c, tiesplit = tiesplit1)
+  output.distr <- prestondistr(input.c, truncate = truncate1)
   output.ve.fit <- veiledspec(output.fit)
   output.ve.distr <- veiledspec(output.distr)
-  
   
   mSetObj$analset$result$fisher$name <- "Species Abundance Model"
   mSetObj$analset$result$fisher$type <- "Fisher's logseries"
@@ -168,68 +111,26 @@ AbundanceModel <- function(mSetObj = NA, data = "false", community = "", tiespli
   write.csv(mSetObj$analset$result$log_likelihood$coeffi, "Max_likelihood coefficients.csv")
   write.csv(mSetObj$analset$result$log_likelihood$frequencies, "Max_likelihood frequencies by Octave.csv")
   write.csv(mSetObj$analset$result$log_likelihood$output.r, "Max_likelihood_Total extrapolated richness.csv")
-  
-  
-#  if (type == "NULL") {
-#      cat("Fisher's logseries")
-#      output <- fisherfit(input.2)
-#  } else if (type == "Split or Keep") {
-#    if (community == "NULL") {
-#        cat("Split the ties or keep all limit cases in the lower octave")
-#        output <- prestonfit(input.2, tiesplit = tiesplit1)
-#    } else {
-#        cat("Split the ties or keep all limit cases in the lower octave")
-#        output <- prestonfit(colSums(input.2), tiesplit = tiesplit1)
-#    }
-#  } else if (type == "Maximize") {
-#    if (community == "NULL") {
-#      cat("Maximizes truncated log-normal likelihood without binning data")
-#      output <- prestondistr(input.2)
-#    } else {
-#      cat("Maximizes truncated log-normal likelihood without binning data")
-#      output <- prestondistr(colSums(input.2))
-#    }
-#  }
-  
-#  if (type != "NULL") {
-#  output.r <- veiledspec(output)
-#  }
-  
-#  if (type == "NULL") {
-#    mSetObj$analset$result$name <- "Species Abundance Model"
-#    mSetObj$analset$result$type <- "Fisher's logseries"
-#    mSetObj$analset$result$estimate <- output$estimate
-#    mSetObj$analset$result$No.of.species <- sum(output$fisher)
-#    mSetObj$analset$result$freq <- output$fisher
-#    mSetObj$analset$result$output <- output
-#  } else {
-#    mSetObj$analset$result$name <- "Species Abundance Model"
-#    mSetObj$analset$result$type <- output$method
-#    mSetObj$analset$result$fitted <- output$fitted
-#    mSetObj$analset$result$freq <- output$freq
-#    mSetObj$analset$result$coefficients <- output$coefficients
-#    mSetObj$analset$result$output <- output
-#    mSetObj$analset$result$output.r <- output.r
-#  }
-
-#  write.csv(mSetObj$analset$result$output.r, "Extrapolated richness.csv")
-#  write.csv(mSetObj$analset$result$coefficients, "Coefficients.csv")
-  
+    
   return(.set.mSet(mSetObj)) 
-  
 }
 
-#basebar = "Maximized likelihood"
-#add.line = "Both"
-#bar.color = "slateblue"
-#color_text = "NULL"
-#line.color.addFit = "coral"
-#line.color_text = "NULL"
-#imgName = "NULL"
-#format = "png"
-#dpi = 72
-#width = NA
 
+#'Perform Fisher log series distribution histogram plot
+#'@description Fisher log series distribution histogram plot 
+#'@param mSetObj Input name of the created mSet Object
+#'@param bar.color Input bar color of the histogram graph. drop down options are skyblue (default), gray, turquoise, slateblue, seagreen, wheat
+#'@param line.color.addFit Input for, drop down options are red (default), coral, brwon, salmon, tomato, sienna
+#'@param imgName Input the image name
+#'@param format Select the image format, "png" or "pdf", default is "png" 
+#'@param dpi Input the dpi. If the image format is "pdf", users need not define the dpi. For "png" images, 
+#'the default dpi is 72. It is suggested that for high-resolution images, select a dpi of 300.  
+#'@param width Input the width, there are 2 default widths. The first, width=NULL, is 10.5.
+#'The second default is width=0, where the width is 7.2. Otherwise users can input their own width
+#'@author Shiyang Zhao\email{shiyang1@ualberta.ca}
+#'University of Alberta, Canada
+#'License: GNU GPL (>= 2) ######
+#'@export
 AbundanceFisherPlot <- function(mSetObj = NA, bar.color = "NULL", line.color.addFit = "NULL", 
                            imgName, format="png", dpi=72, width=NA) {
   
@@ -255,21 +156,6 @@ AbundanceFisherPlot <- function(mSetObj = NA, bar.color = "NULL", line.color.add
   
   Cairo::Cairo(file=imgName, unit="in", dpi=dpi, width=w, height=h, type=format, bg="white")
   par(xpd=FALSE, mar=c(5.1, 4.1, 4.1, 2.1))
-  #abline(0, 1)
-  
-  
-#  if(color=="NULL") { 
-#    color1 = "blue" #default fill palette is grayscale
-#  } else if (color=="manual") { #manual user entry. Selection of this option causes text box to appear
-#    color1 <- "manual"
-#    color_text1 <- color_text #colors entered by user as string with commas between colors
-#    color_text1 <- gsub("\n", "", color_text1, fixed=TRUE) #Prepare colors list, fixed=TRUE means we are dealing with one string, versus a vector of strings (fixed=FALSE)
-#    color_text1 <- gsub(";", ",", color_text1, fixed=TRUE)
-#    color_text1 <- gsub(" ", "", color_text1, fixed=TRUE)
-#    color_text1 <- unlist(strsplit(color_text1, split=","))
-#  } else { #User selected color palette
-#    color1 <- color #user selects color palette from drop down menu (options are grayscale, viridis, plasma)
-#  }
   
   if(bar.color == "NULL") { 
     bar.color1 = "skyblue" #default fill palette is grayscale
@@ -286,8 +172,8 @@ AbundanceFisherPlot <- function(mSetObj = NA, bar.color = "NULL", line.color.add
   } 
   
   if (line.color.addFit == "NULL") { 
-    line.color.addFit1 = "red" #default fill palette is grayscale
-  } else if (line.color.addFit == "coral") { #manual user entry. Selection of this option causes text box to appear
+    line.color.addFit1 = "red" 
+  } else if (line.color.addFit == "coral") { 
     line.color.addFit1 <- "coral3"
   } else if (line.color.addFit == "brown") { 
     line.color.addFit1 <- "brown4"
@@ -299,95 +185,10 @@ AbundanceFisherPlot <- function(mSetObj = NA, bar.color = "NULL", line.color.add
     line.color.addFit1 <- "sienna1"
   }
   
-
-  #pars <- expand.grid(col = color, stringsAsFactors = FALSE)
-  
-  windows(height = h, width = w)
   plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
        line.col = line.color.addFit1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
   axis(1, labels = T)
   axis(2, las = 2)
-  
-#  if (basebar == "NULL") {
-#    if (add.line == "NULL") {
-#      plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#         line.col = line.color.addFit1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#    } else if (add.line == "Poisson fit") {
-#      plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#           line.col = line.color.addFit1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#      lines(plot_data.fit, line.col = line.color.addPoi1)
-#    } else if (add.line == "Maximized likelihood") {
-#      plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#           line.col = line.color1.addFit1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#      lines(plot_data.distr, line.col = line.color.addMax1)
-#    } else if (add.line == "Both") {
-#      plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#           line.col = line.color.addFit1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#      lines(plot_data.fit, line.col = line.color.addPoi1)
-#      lines(plot_data.distr, line.col = line.color.addMax1)
-#    }
-#  } else if (basebar == "Possion fit") {
-#    if (add.line == "NULL") {
-#      plot(plot_data.fit, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#           line.col = line.color.addPoi1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#    } else if (add.line == "Maximized likelihood") {
-#      plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#           line.col = line.color.addPoi1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#      lines(plot_data.distr, line.col = line.color.addMax1)
-#    } else if (add.line == "Fisher alpha") {
-#      plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#           line.col = line.color.addPoi1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#      lines(plot_data.fisher, line.col = line.color.addFit1)
-#    } else if (add.line == "Both") {
-#      plot(plot_data.fisher, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#           line.col = line.color.addPoi1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#      axis(1, labels = T)
-#      axis(2, las = 2)
-#      lines(plot_data.fisher, line.col = line.color.addFit1)
-#      lines(plot_data.distr, line.col = line.color.addMax1)
-#    }
-# } else if (basebar == "Maximized likelihood") {
-#      if (add.line == "NULL") {
-#        plot(plot_data.distr, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#             line.col = line.color.addMax1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#        axis(1, labels = T)
-#        axis(2, las = 2)
-#      } else if (add.line == "Possion fit") {
-#        plot(plot_data.distr, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#             line.col = line.color.addMax1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#        axis(1, labels = T)
-#        axis(2, las = 2)
-#        lines(plot_data.fit, line.col = line.color.addPoi1)
-#      } else if (add.line == "Fisher alpha") {
-#        plot(plot_data.distr, ann = T, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n",
-#             line.col = line.color.addMax1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#        axis(1, labels = T)
-#        axis(2, las = 2)
-#        lines(plot_data.fisher, line.col = line.color.addFit1)
-#      } else if (add.line == "Both") {
-#        plot(plot_data.distr, #ann = T, axes = F, pch = 17, 
-#             lty = 1, lwd = 2, cex = 2, xant = "n", yant = "n", labels = F,
-#             line.col = line.color.addMax1, bar.col = bar.color1, xlab = "Frequency", ylab = "Species")
-#        axis(1, labels = F)
-#        axis(2, las = 2)
-#        lines(plot_data.fisher, line.col = line.color.addFit1)
-#        lines(plot_data.fit, line.col = line.color.addPoi1)
-#    }
-# }
   
   dev.off()
   
@@ -396,17 +197,22 @@ AbundanceFisherPlot <- function(mSetObj = NA, bar.color = "NULL", line.color.add
 
 
 
-#bar.color = "wheat"
-#color_text = "NULL"
-#line.color = "NULL"
-#line.color_text = "NULL"
-#line.color.addPoi = "olive"
-#line.color.addMax = "hotpink"
-#imgName = "NULL"
-#format = "png"
-#dpi = 72
-#width = NA
-
+#'Perform Preston lognormal distribution histogram plot
+#'@description lognormal distribution histogram plot 
+#'@param mSetObj Input name of the created mSet Object
+#'@param bar.color Input bar color of the histogram graph. drop down options are skyblue (default), gray, turquoise, slateblue, seagreen, wheat
+#'@param line.color.addPoi Input for Preston’s lognormal model trend line (traditional binning), drop down options are green (default), olive, springgreen, yellowgreen, gold, orange
+#'@param line.color.addPoi Input for Preston’s lognormal model trend line (without binning), drop down options are purple (default), violetred, orchid, maroon, hotpink, deeppink
+#'@param imgName Input the image name
+#'@param format Select the image format, "png" or "pdf", default is "png" 
+#'@param dpi Input the dpi. If the image format is "pdf", users need not define the dpi. For "png" images, 
+#'the default dpi is 72. It is suggested that for high-resolution images, select a dpi of 300.  
+#'@param width Input the width, there are 2 default widths. The first, width=NULL, is 10.5.
+#'The second default is width=0, where the width is 7.2. Otherwise users can input their own width
+#'@author Shiyang Zhao\email{shiyang1@ualberta.ca}
+#'University of Alberta, Canada
+#'License: GNU GPL (>= 2) ######
+#'@export
 AbundancePrestPlot <- function(mSetObj=NA, bar.color="NULL", line.color.addPoi = "NULL", line.color.addMax = "NULL", imgName, format="png", dpi=72, width=NA) {
   
   library(vegan)
@@ -477,22 +283,13 @@ AbundancePrestPlot <- function(mSetObj=NA, bar.color="NULL", line.color.addPoi =
   } else if (line.color.addMax == "deeppink") { 
     line.color.addMax1 <- "deeppink1"
   } 
-  
-  
-  #pars <- expand.grid(col = color1, stringsAsFactors = FALSE)
-  #xant = "n", yant = "n",
-  windows(height = h, width = w)
-  #if (color1 == "NULL") {
+
   plot(plot_data.distr, ann = F, pch = 17, lty = 1, lwd = 2, cex = 2, yaxt = "n", xaxt = "n", labels = F,
        line.col = line.color.addPoi1, bar.col = bar.color1,  ylab = "Species")
-  #axis(1, labels = T, xlab = "Frequency",)
+  #axis(1, labels = T, xlab = "Frequency")
   #axis(2, las = 2)    
   lines(plot_data.fit, line.col = line.color.addMax1)
-#  #} else {
-# #  plot(plot_data.distr, ann = F, axes = F, pch = 17, lty = 1, lwd = 2, cex = 2, bar.col = bar.color1)
-#  #  lines(plot_data.fit, line.col = line.color)
-#  #}
-#  
+
   dev.off()
   
   return(.set.mSet(mSetObj))
