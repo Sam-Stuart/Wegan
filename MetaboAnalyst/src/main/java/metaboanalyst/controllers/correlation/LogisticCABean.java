9bacf572a18ed1dcffb38e616de3c4debd69c8ef
gsykes <gsykes@ualberta.ca>
2022-08-06 17:28:24 -0600

    LogisticCABean.java 202208-06; add logModelType (staticdropdown); doLabelXtickRotate (checkbox); logPaletteOpts (staticdropdown); doPlotLegHoriz (checkbox); logPlotLegPosOpts (staticdropdown); corPlotTitle; corPlotXlab; corPlotYlab; modified CAUtils.corrLog1Btn_action and CAUtils.corrLogBtn2_action (default args)

--- MetaboAnalyst/src/main/java/metaboanalyst/controllers/correlation/LogisticCABean.java
+++ MetaboAnalyst/src/main/java/metaboanalyst/controllers/correlation/LogisticCABean.java
@@ -99,11 +99,192 @@ public class LogisticCABean implements Serializable {
         return corrPolyResults;
     }
     
+ 
+    
+    
+    
+    
+    
+    
+  // CHECK BOX for using normalized data (default) or original data
+    private boolean doOriginal = false;
+
+    public boolean isdoOriginal() {
+        return doOriginal;
+    }
+
+    public void setdoOriginal(boolean doOriginal) {
+        this.doOriginal = doOriginal;
+    }
+  // CHECK BOX for adding (default) or omitting equation to plot (at top), see correlation_linear.R 
+  // when >1 of rsq, eq, & rsqadj are checked, values are seperated by " | " 
+     private boolean doPlotEq = false;
+
+    public boolean isdoPlotEq() {
+        return doPlotEq;
+    }
+
+    public void setdoPlotEq(boolean doPlotEq) {
+        this.doPlotEq = doPlotEq;
+    } 
+  // CHECK BOX
+     private boolean doPlotRsq = false;
+
+    public boolean isdoPlotRsq() {
+        return doPlotRsq;
+    }
+
+    public void setdoPlotRsq(boolean doPlotRsq) {
+        this.doPlotRsq = doPlotRsq;
+    }     
+  // CHECK BOX
+     private boolean doPlotRsqAdj = false;
+
+    public boolean isdoPlotRsqAdj() {
+        return doPlotRsqAdj;
+    }
+
+    public void setdoPlotRsqAdj(boolean doPlotRsqAdj) {
+        this.doPlotRsqAdj = doPlotRsqAdj;
+    }  
+  // CHECK BOX 
+     private boolean doPlotConfInt = false;
+
+    public boolean isdoPlotConfInt() {
+        return doPlotConfInt;
+    }
+
+    public void setdoPlotConfInt(boolean doPlotConfInt) {
+        this.doPlotConfInt = doPlotConfInt;
+    }    
+    
+ //STATIC DROPDOWN    
+    private String logModelType = "NULL"; //FUNCTION CORRESPONDS WITH applicationBean1.logModType
+
+    public String getLogModelType() {
+        return logModelType;
+    }
+
+    public void setLogModelType(String logModelType) {
+        this.logModelType = logModelType;
+    }
+    
+    // CHECK BOX 
+     private boolean doLabelXtickRotate = false;
+
+    public boolean isdoLabelXtickRotate() {
+        return doLabelXtickRotate;
+    }
+
+    public void setdoLabelXtickRotate(boolean doLabelXtickRotate) {
+        this.doLabelXtickRotate = doLabelXtickRotate;
+    }
+    
+ //STATIC DROPDOWN for selecting colour:   replaced: corColorOpts
+    private String logPaletteOpts = "NULL"; //FUNCTION CORRESPONDS WITH applicationBean1.logColorPaletteOpts
+
+    public String getLogPaletteOpts() {
+        return logPaletteOpts; 
+    }
+
+    public void setLogPaletteOpts(String logPaletteOpts) {
+        this.logPaletteOpts = logPaletteOpts;
+    }
+
+    // CHECK BOX 
+     private boolean doPlotLegHoriz = false;
+
+    public boolean isdoPlotLegHoriz() {
+        return doPlotLegHoriz; 
+    }
+
+    public void setdoPlotLegHoriz(boolean doPlotLegHoriz) {
+        this.doPlotLegHoriz = doPlotLegHoriz;
+    }
+    
+ //STATIC DROPDOWN for selecting colour of line on plot
+    private String logPlotLegPosOpts = "NULL"; //FUNCTION CORRESPONDS WITH applicationBean1.logLegPosOpts
+
+    public String getLogPlotLegPosOpts() {
+        return logPlotLegPosOpts;
+    }
+
+    public void setLogPlotLegPosOpts(String logPlotLegPosOpts) {
+        this.logPlotLegPosOpts = logPlotLegPosOpts;
+    }
+    
+  
+  // TEXT BOX 
+    private String corPlotTitle = " ";
+    
+    public String getCorPlotTitle() {
+        return corPlotTitle;
+    }
+
+    public void setCorPlotTitle(String corPlotTitle) {
+        this.corPlotTitle = corPlotTitle;
+    }
+ 
+     // TEXT BOX 
+    private String corPlotXlab = " ";
+    
+    public String getCorPlotXlab() {
+        return corPlotXlab;
+    }
+
+    public void setCorPlotXlab(String corPlotXlab) {
+        this.corPlotXlab = corPlotXlab;
+    }       
+    
+ // TEXT BOX 
+    private String corPlotYlab = " ";
+    
+    public String getCorPlotYlab() {
+        return corPlotYlab;
+    }
+
+    public void setCorPlotYlab(String corPlotYlab) {
+        this.corPlotYlab = corPlotYlab;
+    } 
+    
+    
+//     facA="NULL", # dropdown
+//                         predtext="NULL", # textbox
+//                         type="multinomial",
+//                         reference="NULL", # dropdown
+//                         ordertext="NULL" # textbox; or
+    
         // ACTION BUTTONS //
-    public void corrLogBtn_action() {
+    public void corrLogBtn1_action() {
+        CAUtils.CreateLogisticModel(sb, 
+                responseLevelVar,indInput,
+                  logModelType, responseLevelVar, indInput);
+        CAUtils.PlotLogisticEffectCA(sb, logModelType, doPlotConfInt,
+                 corPlotTitle, corPlotXlab, corPlotYlab,               
+                doLabelXtickRotate, logPaletteOpts, doPlotLegHoriz, logPlotLegPosOpts,
+                sb.getCurrentImage("corr_log_eff"), "png", 72);
+    }
+    
+   // ACTION BUTTONS //
+    public void corrLogBtn2_action() {
+        CAUtils.CreateLogisticModel(sb, 
+               responseLevelVar,indInput,
+               logModelType, responseLevelVar, indInput);
+//                columnNameA, indInput);
+        CAUtils.PlotLogisticROCCA(sb, 
+                corPlotTitle, corPlotXlab, corPlotYlab,
+                sb.getCurrentImage("corr_log_roc"), "png", 72);
+    }
+    
+    
+    
+    
+    
+        // ACTION BUTTONS //
+//    public void corrLogBtn_action() {
 //        CAUtils.CreateSVMModel(sb, columnNameA, indInput);
 //        CAUtils.PlotSVMCA(sb, sb.getCurrentImage("corr_svm"), "png", 72);
-    }
+//    }
 //    // ACTION BUTTONS //
 //    public void corrPolyPredBtn_action() {
 //        System.out.println("Inside poly");
