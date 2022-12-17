b207f6554d6f59683056b3d4e98feda515ad040c
Hieu Nguyen <axelnguyen0701@gmail.com>
2022-12-17 16:03:42 -0700

    Make PiePlot inherits Plot

--- MetaboAnalyst/src/main/java/metaboanalyst/controllers/plotting/PiePlotBean.java
+++ MetaboAnalyst/src/main/java/metaboanalyst/controllers/plotting/PiePlotBean.java
@@ -13,11 +13,8 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.model.SelectItem;
-import metaboanalyst.controllers.ApplicationBean1;
-import metaboanalyst.controllers.SessionBean1;
 import metaboanalyst.rwrappers.PlottingUtils;
-import metaboanalyst.utils.DataUtils;
-import java.util.ArrayList; // import the ArrayList class
+import java.util.ArrayList;
 
 /**
  *
@@ -25,40 +22,23 @@ import java.util.ArrayList; // import the ArrayList class
  */
 @ManagedBean(name = "piePlotBean")
 @ViewScoped
-public class PiePlotBean implements Serializable {
+public class PiePlotBean extends PlotBean implements Serializable {
 
-    private final ApplicationBean1 ab = (ApplicationBean1) DataUtils.findBean("applicationBean1");
-    private final SessionBean1 sb = (SessionBean1) DataUtils.findBean("sessionBean1");
     private final SelectItem[] aggregateOpts;
     private ArrayList<SelectItem> numericBoxColumnOpts;
-
     private String aggregateFunChosen;
-    private String labx;
-    private String laby;
-    private String title;
     private String facA = getNumericBoxColumnOpts().get(0).getLabel();
 
-  
-
     public PiePlotBean() {
+        super();
         aggregateOpts = new SelectItem[5];
         aggregateOpts[0] = new SelectItem("mean", "Mean");
         aggregateOpts[1] = new SelectItem("sum", "Sum");
         aggregateOpts[2] = new SelectItem("length", "Count");
         aggregateOpts[3] = new SelectItem("min", "Min");
         aggregateOpts[4] = new SelectItem("max", "Max");
-
-//        SelectItem[] fontSizeOpts = new SelectItem[5];
-//        fontSizeOpts[0] = new SelectItem(12, "Extra Small");
-//        fontSizeOpts[1] = new SelectItem(16, "Small");
-//        fontSizeOpts[2] = new SelectItem(20, "Medium");
-//        fontSizeOpts[3] = new SelectItem(24, "Large");
-//        fontSizeOpts[4] = new SelectItem(32, "Extra Large");
-//        
-//        this.textFontSizeOpts = fontSizeOpts.clone();
-//        this.axisFontSizeOpts = fontSizeOpts.clone();
     }
-    
+
     public String getFacA() {
         return facA;
     }
@@ -66,10 +46,10 @@ public class PiePlotBean implements Serializable {
     public void setFacA(String facA) {
         this.facA = facA;
     }
-    
+
     public void setNumericBoxColumnOpts() {
         List<String> columns = Arrays.asList(PlottingUtils.GetNumericDataColumnsBoxPlt(sb));
-        this.numericBoxColumnOpts = new ArrayList<SelectItem>();
+        this.numericBoxColumnOpts = new ArrayList<>();
         columns.forEach((e) -> {
             this.numericBoxColumnOpts.add(new SelectItem(e, e));
         });
@@ -94,31 +74,8 @@ public class PiePlotBean implements Serializable {
         this.aggregateFunChosen = aggregateFunChosen;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setLaby(String laby) {
-        this.laby = laby;
-    }
-
-    public String getLaby() {
-        return laby;
-    }
-
-    public void setLabx(String labx) {
-        this.labx = labx;
-    }
-
-    public String getLabx() {
-        return labx;
-    }
-
-    public void pieBtn_action() {
+    @Override
+    public void button_action() {
         PlottingUtils.CreatePieChart(sb, this.facA, "NULL", labx, laby, "NULL", title, "NULL", this.aggregateFunChosen, false);
         PlottingUtils.PlotPieChart(sb, sb.getNewImage("plot_pie_chart"), "png", 72);
     }
