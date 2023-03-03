6866abf9950f44978bdccdad88c21eb80ffa767d
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-03-02 18:40:47 -0700

    Add network graph with colors and stroke width

---
+++ MetaboAnalyst/src/main/webapp/Secure/correlation/NetworkGraph.js
@@ -0,0 +1,248 @@
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/force-directed-graph
+let URL = document.getElementById("mydir").value;
+d3.json("/MetaboAnalyst" + URL + "/corr_ann_nid.json").then(function (_data) {
+    const div = document.getElementById("my_dataviz");
+
+    const nodes = _data.nodes.ID.map((e, i) => {
+        return {
+            id: e,
+            name: _data.nodes.name[i],
+            color: _data.nodes.cols[i],
+        };
+    });
+    const links = _data.edges.source.map((e, i) => {
+        return {
+            source: e,
+            target: _data.edges.target[i],
+            color: _data.edges.target[i],
+            strokeWidth: _data.edges.weight_line_thickness[i],
+        };
+    });
+
+    const tooltip = d3
+        .select("#my_dataviz")
+        .append("div")
+        .style("opacity", 0)
+        .style("position", "absolute")
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px");
+
+    const data = { nodes, links };
+    console.log(data);
+
+    const svg = ForceGraph(data, {
+        tooltip,
+        nodeId: (d) => d.id,
+        colors: _data.nodes.cols,
+        nodeTitle: (d) => `${d.name}`,
+        linkStrokeWidth: (d) => d.strokeWidth,
+    });
+
+    div.appendChild(svg);
+
+    d3.select("#my_dataviz_title").html(_data.main);
+});
+// let data;
+// d3.json(link2).then(function (_data) {
+//     data = _data;
+//     data.nodes = data.nodes.map((e, i) => ({ ...e, id: i }));
+//     const div = document.getElementById("my_dataviz");
+//     const tooltip = d3
+//         .select("#my_dataviz")
+//         .append("div")
+//         .style("opacity", 0)
+//         .style("position", "absolute")
+//         .attr("class", "tooltip")
+//         .style("background-color", "white")
+//         .style("border", "solid")
+//         .style("border-width", "1px")
+//         .style("border-radius", "5px")
+//         .style("padding", "10px");
+//     const svg = ForceGraph(data, {
+//         width: div.offsetWidth,
+//         height: 600,
+//         nodeGroup: (d) => d.group,
+//         nodeId: (d) => d.id,
+//         nodeTitle: (d) => `${d.name}\n${d.group}`,
+//         tooltip,
+//     });
+
+//     div.appendChild(svg);
+// });
+
+function ForceGraph(
+    {
+        nodes, // an iterable of node objects (typically [{id}, …])
+        links, // an iterable of link objects (typically [{source, target}, …])
+    },
+    {
+        nodeId = (d) => d.id, // given d in nodes, returns a unique identifier (string)
+        nodeGroup, // given d in nodes, returns an (ordinal) value for color
+        nodeGroups, // an array of ordinal values representing the node groups
+        nodeTitle, // given d in nodes, a title string
+        nodeFill = "currentColor", // node stroke fill (if not using a group color encoding)
+        nodeStroke = "#fff", // node stroke color
+        nodeStrokeWidth = 1.5, // node stroke width, in pixels
+        nodeStrokeOpacity = 1, // node stroke opacity
+        nodeRadius = 5, // node radius, in pixels
+        nodeStrength,
+        linkSource = ({ source }) => source, // given d in links, returns a node identifier string
+        linkTarget = ({ target }) => target, // given d in links, returns a node identifier string
+        linkStroke = "#999", // link stroke color
+        linkStrokeOpacity = 0.6, // link stroke opacity
+        linkStrokeWidth = 1.5, // given d in links, returns a stroke width in pixels
+        linkStrokeLinecap = "round", // link stroke linecap
+        linkStrength,
+        colors = d3.schemeTableau10, // an array of color strings, for the node groups
+        width = 640, // outer width, in pixels
+        height = 400, // outer height, in pixels
+        invalidation, // when this promise resolves, stop the simulation
+        tooltip,
+    } = {}
+) {
+    // Compute values.
+    const N = d3.map(nodes, nodeId).map(intern);
+    const LS = d3.map(links, linkSource).map(intern);
+    const LT = d3.map(links, linkTarget).map(intern);
+    if (nodeTitle === undefined) nodeTitle = (_, i) => N[i];
+    const T = nodeTitle == null ? null : d3.map(nodes, nodeTitle);
+    const G = nodeGroup == null ? null : d3.map(nodes, nodeGroup).map(intern);
+    const W =
+        typeof linkStrokeWidth !== "function"
+            ? null
+            : d3.map(links, linkStrokeWidth);
+
+    const L =
+        typeof linkStroke !== "function" ? null : d3.map(links, linkStroke);
+
+    // Replace the input nodes and links with mutable objects for the simulation.
+    nodes = d3.map(nodes, (_, i) => ({ id: N[i] }));
+    links = d3.map(links, (_, i) => ({ source: LS[i], target: LT[i] }));
+
+    // Compute default domains.
+    if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
+
+    // Construct the scales.
+    const color =
+        nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
+
+    // Construct the forces.
+    const forceNode = d3.forceManyBody();
+    const forceLink = d3.forceLink(links).id(({ index: i }) => N[i]);
+    if (nodeStrength !== undefined) forceNode.strength(nodeStrength);
+    if (linkStrength !== undefined) forceLink.strength(linkStrength);
+
+    const simulation = d3
+        .forceSimulation(nodes)
+        .force("link", forceLink)
+        .force("charge", forceNode)
+        .force("center", d3.forceCenter())
+        .on("tick", ticked);
+
+    const svg = d3
+        .create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+    const link = svg
+        .append("g")
+        .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
+        .attr("stroke-opacity", linkStrokeOpacity)
+        .attr(
+            "stroke-width",
+            typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null
+        )
+        .attr("stroke-linecap", linkStrokeLinecap)
+        .selectAll("line")
+        .data(links)
+        .join("line");
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    const mouseover = function (event, d) {
+        tooltip.style("opacity", 1);
+    };
+
+    const mousemove = function (event, d) {
+        tooltip
+            .html(`Character:${T[d.index]}`)
+            .style("left", event.pageX + 5 + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+            .style("top", event.pageY + "px");
+    };
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    const mouseleave = function (event, d) {
+        tooltip.transition().duration(200).style("opacity", 0);
+    };
+
+    const node = svg
+        .append("g")
+        .attr("fill", nodeFill)
+        .attr("stroke", nodeStroke)
+        .attr("stroke-opacity", nodeStrokeOpacity)
+        .attr("stroke-width", nodeStrokeWidth)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", nodeRadius)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+        .call(drag(simulation));
+
+    if (W) link.attr("stroke-width", ({ index: i }) => W[i]);
+    if (L) link.attr("stroke", ({ index: i }) => L[i]);
+    node.attr("fill", ({ index: i }) => colors[i]);
+    // if (T) node.append("title").text(({ index: i }) => T[i]);
+    if (invalidation != null) invalidation.then(() => simulation.stop());
+
+    function intern(value) {
+        return value !== null && typeof value === "object"
+            ? value.valueOf()
+            : value;
+    }
+
+    function ticked() {
+        link.attr("x1", (d) => d.source.x)
+            .attr("y1", (d) => d.source.y)
+            .attr("x2", (d) => d.target.x)
+            .attr("y2", (d) => d.target.y);
+
+        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    }
+
+    function drag(simulation) {
+        function dragstarted(event) {
+            if (!event.active) simulation.alphaTarget(0.3).restart();
+            event.subject.fx = event.subject.x;
+            event.subject.fy = event.subject.y;
+        }
+
+        function dragged(event) {
+            event.subject.fx = event.x;
+            event.subject.fy = event.y;
+        }
+
+        function dragended(event) {
+            if (!event.active) simulation.alphaTarget(0);
+            event.subject.fx = null;
+            event.subject.fy = null;
+        }
+
+        return d3
+            .drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+    }
+
+    return Object.assign(svg.node(), { scales: { color } });
+}
