33a301e01abd650d7b2fab4058287bd1864300d9
Xin Zhao <xinzhao@Xins-MacBook-Pro.local>
2023-04-20 10:18:13 -0600

    add comments to plotting codes

--- WGCNA_functions/taxo_02_autoNetworkDetect.R
+++ WGCNA_functions/taxo_02_autoNetworkDetect.R
@@ -17,7 +17,7 @@ findSoftThreshold <- function(mSetObj = NA,
     
     library(WGCNA) 
     library(tidyverse) 
-    source("./Func_WGCNA/general_data_utils.R") 
+    source("./WGCNA_functions/taxo_00_generalDataUtils.R")  
     
     mSetObj <- .get.mSet(mSetObj) 
     
@@ -39,30 +39,54 @@ findSoftThreshold <- function(mSetObj = NA,
     sft <- WGCNA::pickSoftThreshold(datExpr, 
                                     powerVector = powers, 
                                     verbose = 5)
-    # Plot 
-    pdf(file)
-    par(mfrow = c(1, 2))     
+    
+    
+    # Plot for visual investigation of soft thresholds 
+    pdf(file) # PDF device 
+    par(mfrow = c(1, 2)) # Layout of two plots
     cex1 <- 0.9 
     # Scale-free topology fit index vs Soft-threshold power 
-    plot(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2], type = "n") 
-    text(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2],
+    plot(sft$fitIndices[, 1], 
+         -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2], 
+         type = "n") # for no plotting 
+    text(sft$fitIndices[, 1], 
+         -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2],
         labels = powers, 
-        cex = cex1, col = "red") 
-    abline(h = 0.90, col = "red") 
+        cex = cex1, 
+        col = "red") 
+    abline(h = 0.90, 
+           col = "red") 
+    
     # Mean connectivity vs Soft-threshold power 
-    plot(sft$fitIndices[, 1], sft$fitIndices[, 5],
-        xlab = "Soft threshold (power)", ylab = "Mean connectivity", type = "n",
+    plot(sft$fitIndices[, 1], 
+         sft$fitIndices[, 5],
+        xlab = "Soft threshold (power)", 
+        ylab = "Mean connectivity", 
+        type = "n", # for no plotting 
         main = paste("Mean connectivity")) 
-    text(sft$fitIndices[, 1], sft$fitIndices[, 5], labels = powers, cex = cex1, col = "red") 
+    text(sft$fitIndices[, 1], 
+         sft$fitIndices[, 5], 
+         labels = powers, 
+         cex = cex1, 
+         col = "red") 
     
     dev.off()
     
+    # ggplot - scatter plots side-by-side 
+    
+    
+    
+    
+    
+    # Save the resulting figure file name to mSet obejct 
     mSetObj$imgSet$softThreshold <- file
     
     .set.mSet(mSetObj)
 
 } 
 
+
+# To code for module-detection function after obtaining optimal soft threshold ...
 
 
 #===============================================================================
@@ -71,20 +95,17 @@ findSoftThreshold <- function(mSetObj = NA,
 
 #===============================================================================
 
-load("./mSet_example.RData")
-source("./selfDefined-Functions/dataInputClean_fun.R")
+load("./WGCNA_data/mSet_example.RData")
+source("./WGCNA_functions/taxo_01_dataInputClean.R") 
 
 # Call my function 
 .on.public.web <- FALSE 
 
 mSetObj <- make.exprSet(mSetObj = mSetObj_example)  
 
-
 # Call the new function 
 # debug(findSoftThreshold)
-
-findSoftThreshold(mSetObj = mSetObj, file = "sfthres.pdf") 
-
+findSoftThreshold(mSetObj = mSetObj, file = "./WGCNA_output/softhreshold.pdf")  
 # undebug(findSoftThreshold)
 
 
