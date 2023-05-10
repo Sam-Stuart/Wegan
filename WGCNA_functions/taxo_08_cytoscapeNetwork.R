008093d0a73587cad73c6f8749e07e1ee6ceb606
Xin Zhao <xinzhao@Xins-MacBook-Pro.local>
2023-05-10 14:21:39 -0600

    phylogeny initial R and markdown files

---
+++ WGCNA_functions/taxo_08_cytoscapeNetwork.R
@@ -0,0 +1,10 @@
+# Draw the gene network using the Cytoscape R package, RCy3 
+
+# Install the package 
+# BiocManager::install("RCy3") 
+
+library(RCy3) 
+
+# Get started 
+RCy3::cytoscapePing() 
+
