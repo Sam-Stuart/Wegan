b390ea9910cbcee3369f2a11ef4c0506c8dd733c
xinzhao <xinzhao@localhost>
2023-05-10 10:52:41 -0600

    commented by Gina

--- MetaboAnalyst/src/main/java/metaboanalyst/controllers/taxonomy/WGCNABean.java
+++ MetaboAnalyst/src/main/java/metaboanalyst/controllers/taxonomy/WGCNABean.java
@@ -38,80 +38,80 @@ public class OAPCOABean implements Serializable {
     
     
     //TABLES FOR DOWNLOAD
-    private User usr = sb.getCurrentUser();
-    private String usrName = usr.getName();
-    
-    
-    private String file2DSampleScores = "pcoa_2D_sample_scores.csv";
-    private String file2DSampleScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file2DSampleScores + "\">" + file2DSampleScores + "</a>";
- 
-    public String getFile2DSampleScoresPath() {
-        return file2DSampleScoresPath;
-    }
-        
-    public void setFile2DSampleScoresPath(String file2DSampleScoresPath) {
-        this.file2DSampleScoresPath = file2DSampleScoresPath;
-    } 
-    
-    
-    
-    private String file2DColScores = "pcoa_2D_variable_scores.csv";
-    private String file2DColScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file2DColScores + "\">" + file2DColScores + "</a>";
- 
-    public String getFile2DColScoresPath() {
-        return file2DColScoresPath;
-    }
-        
-    public void setFile2DColScoresPath(String file2DColScoresPath) {
-        this.file2DColScoresPath = file2DColScoresPath;
-    } 
-    
-    
-    private String file2DEnvScores = "pcoa_2D_constraining_variable_scores.csv"; //TABLE SHOULD ONLY POP UP IF ENVDATA IS UPLOADED
-    private String file2DEnvScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file2DEnvScores + "\">" + file2DEnvScores + "</a>";
- 
-    public String getFile2DEnvScoresPath() {
-        return file2DEnvScoresPath;
-    }
-        
-    public void setFile2DEnvScoresPath(String file2DEnvScoresPath) {
-        this.file2DEnvScoresPath = file2DEnvScoresPath;
-    } 
-    
-    
-    private String file3DSampleScores = "pcoa_3D_sample_scores.csv";
-    private String file3DSampleScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file3DSampleScores + "\">" + file3DSampleScores + "</a>";
- 
-    public String getFile3DSampleScoresPath() {
-        return file3DSampleScoresPath;
-    }
-        
-    public void setFile3DSampleScoresPath(String file3DSampleScoresPath) {
-        this.file3DSampleScoresPath = file3DSampleScoresPath;
-    } 
-    
-    
-    private String file3DColScores = "pcoa_3D_variable_scores.csv";
-    private String file3DColScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file3DColScores + "\">" + file3DColScores + "</a>";
- 
-    public String getFile3DColScoresPath() {
-        return file3DColScoresPath;
-    }
-        
-    public void setFile3DColScoresPath(String file3DColScoresPath) {
-        this.file3DColScoresPath = file3DColScoresPath;
-    } 
-    
-    
-    private String fileScree = "pcoa_scree_data.csv";
-    private String fileScreePath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + fileScree + "\">" + fileScree + "</a>";
- 
-    public String getFileScreePath() {
-        return fileScreePath;
-    }
-        
-    public void setFileScreePath(String fileScreePath) {
-        this.fileScreePath = fileScreePath;
+//    private User usr = sb.getCurrentUser();
+//    private String usrName = usr.getName();
+//    
+//    
+//    private String file2DSampleScores = "pcoa_2D_sample_scores.csv";
+//    private String file2DSampleScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file2DSampleScores + "\">" + file2DSampleScores + "</a>";
+// 
+//    public String getFile2DSampleScoresPath() {
+//        return file2DSampleScoresPath;
+//    }
+//        
+//    public void setFile2DSampleScoresPath(String file2DSampleScoresPath) {
+//        this.file2DSampleScoresPath = file2DSampleScoresPath;
+//    } 
+//    
+//    
+//    
+//    private String file2DColScores = "pcoa_2D_variable_scores.csv";
+//    private String file2DColScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file2DColScores + "\">" + file2DColScores + "</a>";
+// 
+//    public String getFile2DColScoresPath() {
+//        return file2DColScoresPath;
+//    }
+//        
+//    public void setFile2DColScoresPath(String file2DColScoresPath) {
+//        this.file2DColScoresPath = file2DColScoresPath;
+//    } 
+//    
+//    
+//    private String file2DEnvScores = "pcoa_2D_constraining_variable_scores.csv"; //TABLE SHOULD ONLY POP UP IF ENVDATA IS UPLOADED
+//    private String file2DEnvScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file2DEnvScores + "\">" + file2DEnvScores + "</a>";
+// 
+//    public String getFile2DEnvScoresPath() {
+//        return file2DEnvScoresPath;
+//    }
+//        
+//    public void setFile2DEnvScoresPath(String file2DEnvScoresPath) {
+//        this.file2DEnvScoresPath = file2DEnvScoresPath;
+//    } 
+//    
+//    
+//    private String file3DSampleScores = "pcoa_3D_sample_scores.csv";
+//    private String file3DSampleScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file3DSampleScores + "\">" + file3DSampleScores + "</a>";
+// 
+//    public String getFile3DSampleScoresPath() {
+//        return file3DSampleScoresPath;
+//    }
+//        
+//    public void setFile3DSampleScoresPath(String file3DSampleScoresPath) {
+//        this.file3DSampleScoresPath = file3DSampleScoresPath;
+//    } 
+//    
+//    
+//    private String file3DColScores = "pcoa_3D_variable_scores.csv";
+//    private String file3DColScoresPath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + file3DColScores + "\">" + file3DColScores + "</a>";
+// 
+//    public String getFile3DColScoresPath() {
+//        return file3DColScoresPath;
+//    }
+//        
+//    public void setFile3DColScoresPath(String file3DColScoresPath) {
+//        this.file3DColScoresPath = file3DColScoresPath;
+//    } 
+//    
+//    
+//    private String fileScree = "pcoa_scree_data.csv";
+//    private String fileScreePath = "<a target='_blank' href = \"/MetaboAnalyst/resources/users/" + usrName + File.separator + fileScree + "\">" + fileScree + "</a>";
+// 
+//    public String getFileScreePath() {
+//        return fileScreePath;
+//    }
+//        
+//    public void setFileScreePath(String fileScreePath) {
+//        this.fileScreePath = fileScreePath;
     } 
     
     //TEXT BOX  
@@ -138,26 +138,18 @@ public class OAPCOABean implements Serializable {
     }
 
     //STATIC DROPDOWN
-    private String vegdistOpts = "NULL"; //FUNCTION CORRESPONDS WITH applicationBean1.vegdistMeasureOpts
+    private int taxPowerOpts = 6; //FUNCTION CORRESPONDS WITH applicationBean1.vegdistMeasureOpts
     
-    public String getVegdistOpts() {
-        return vegdistOpts;
+    public int setTaxPowerOpts() {
+        return taxPowerOpts;
     }
 
-    public void setVegdistOpts(String vegdistOpts) {
-        this.vegdistOpts = vegdistOpts;
+    public void gettaxPowerOpts(int taxPowerOpts) {
+        this.taxPowerOpts = taxPowerOpts;
     }
 
     
-    private String ordStressDimOpts = "NULL"; //FUNCTION CORRESPONDS WITH applicationBean1.ordStressDimensionOpts
-    
-    public String getOrdStressDimOpts() {
-        return ordStressDimOpts;
-    }
-
-    public void setOrdStressDimOpts(String ordStressDimOpts) {
-        this.ordStressDimOpts = ordStressDimOpts;
-    }
+   
 //
 //
 //    private String ordColorOpts = "NULL"; //FUNCTION CORRESPONDS WITH applicationBean1.ordColorPaletteOpts
@@ -228,25 +220,14 @@ public class OAPCOABean implements Serializable {
 //        OAUtils.PlotPCOAstressOrdination(sb, sb.getNewImage("ord_pcoa_stress"), "png", 72);
 //        OAUtils.PlotPCOAscreeOrdination(sb, sb.getNewImage("ord_pcoa_scree"), "png", 72);    
 //    }
+//    OAUtils.PlotPCOA2DOrdination(sb, ellipseOpts, var_arrowsOpts, env_arrowsOpts, env_centOpts, sampleNamesOpts, ordColorOpts, pcoaMetaColumnName, sb.getNewImage("ord_pcoa_2D"), "png", 72);
+
     public void pcoa_2D_Update_action() {
         TaxonomyUtils.MakeWGCNAData(sb, doCustomNorm, 1);
-        TaxonomyUtils.PlotWGCNAGeneNetwork(sb, doCustomNorm, 0, usrName, usrName, 0, usrName, 0, 0);
-        OAUtils.PlotPCOA2DOrdination(sb, ellipseOpts, var_arrowsOpts, env_arrowsOpts, env_centOpts, sampleNamesOpts, ordColorOpts, pcoaMetaColumnName, sb.getNewImage("ord_pcoa_2D"), "png", 72);
-    }
+        TaxonomyUtils.PlotWGCNAGeneNetwork(sb, doCustomNorm);
+        }
     
-    public void pcoa_3D_Update_action() {
-        OAUtils.CreatePCOAOrdination(sb, doOriginal, vegdistOpts, doBinary, doAbundance, envDataCol);
-        OAUtils.PlotPCOA3DOrdination(sb, ordColorOpts, var_arrowsOpts, pcoaMetaColumnName, sb.getNewImage("ord_pcoa_3D"));
-    }
-    
-    public void pcoa_stress_Update_action() {
-        OAUtils.CreatePCOAOrdination(sb, doOriginal, vegdistOpts, doBinary, doAbundance, envDataCol);
-        OAUtils.PlotPCOAstressOrdination(sb, sb.getNewImage("ord_pcoa_stress"), "png", 72);
-    }
-    public void pcoa_scree_Update_action() {
-        OAUtils.CreatePCOAOrdination(sb, doOriginal, vegdistOpts, doBinary, doAbundance, envDataCol);
-        OAUtils.PlotPCOAscreeOrdination(sb, pcoaScreeDimNum, sb.getNewImage("ord_pcoa_scree"), "png", 72);    
-    }
+
 }
 
 
